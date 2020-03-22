4e3d69b7754590b616ab2652d171ff1637592d9d
Martin Honermeyer <maze@strahlungsfrei.de>
2020-03-22 17:09:35 +0100

    Fix image urls when using "local" activestorage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,11 @@ module ApplicationHelper
 
     def attached_image_url(attachment)
         if attachment.attached?
-            ix_image_url(attachment.key)
-        else
-            # FIXME local urls
+            if Rails.application.config.active_storage.service == :local
+                rails_blob_url(attachment)
+            else
+                ix_image_url(attachment.key)
+            end
         end
     end
 end
