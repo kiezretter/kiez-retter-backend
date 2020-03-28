f2430792a809e2aaad7d8d5c588888b45b260537
Martin Honermeyer <maze@strahlungsfrei.de>
2020-03-28 19:49:13 +0100

    Allow destroying business imports in backend

--- app/controllers/business_imports_controller.rb
+++ app/controllers/business_imports_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class BusinessImportsController < ApplicationController
+  before_action :set_business_import, only: %i[show destroy]
+
   def new
     @business_import = BusinessImport.new
   end
@@ -19,11 +21,18 @@ class BusinessImportsController < ApplicationController
     @business_imports = BusinessImport.all
   end
 
-  def show
-    @business_import = BusinessImport.find(params[:id])
+  def show; end
+
+  def destroy
+    @business_import.destroy
+    redirect_to business_imports_url, notice: 'Business import was successfully destroyed.'
   end
 
   private
+
+  def set_business_import
+    @business_import = BusinessImport.find(params[:id])
+  end
 
   def business_import_params
     params.require(:business_import).permit(
