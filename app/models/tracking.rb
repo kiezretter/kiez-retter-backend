e5ea5c341d3e99a5037479d11e52045b3fe02913
Robin Zuschke <robin.l.zuschke@gmail.com>
2020-04-02 15:14:04 +0200

    Rubocop

--- app/models/tracking.rb
+++ app/models/tracking.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Tracking < ApplicationRecord
   belongs_to :business, optional: true
 end
--- db/migrate/20200330145151_allow_null_for_business_id_in_trackings.rb
+++ db/migrate/20200330145151_allow_null_for_business_id_in_trackings.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AllowNullForBusinessIdInTrackings < ActiveRecord::Migration[6.0]
   def change
     change_column_null :trackings, :business_id, true
