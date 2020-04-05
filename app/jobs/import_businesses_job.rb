7b422728243cb0baef42df4894abede9a32713c1
thohee <thomas.heer@posteo.de>
2020-04-06 00:05:22 +0200

    map google place type to kiez-retter business type

--- app/jobs/import_businesses_job.rb
+++ app/jobs/import_businesses_job.rb
@@ -28,14 +28,13 @@ class ImportBusinessesJob < ApplicationJob
       begin
         row_form = row.to_hash
         b = Business.create!({
-          business_type_id: business_type_mapping[row_form['business_type']],
           name: row_form['name'],
           street_address: row_form['street_address'],
           postcode: row_form['postcode'],
           city: row_form['city'],
           business_import: business_import
         }.merge(
-          geo_params(row_form['name'], row_form['street'], row_form['city'], row_form['postcode'])
+          geo_params(business_type_mapping, row_form['name'], row_form['street'], row_form['city'], row_form['postcode'])
         ))
         Funding.create!(funding_type: 0, business: b, link: row_form['url'])
         Owner.create!(business: b, paypal_handle: '', email: '', nick_name: '', first_name: '', last_name: '')
@@ -49,11 +48,42 @@ class ImportBusinessesJob < ApplicationJob
 
   private
 
-  def geo_params(name, street, city, postcode)
-    results = Geocoder.search("#{name} #{street} #{city} #{postcode}")
+  def determine_business_type(business_type_mapping, place_id)
+    if place_id.nil?
+      return business_type_mapping['service']
+    end
+    response = Geocoder.search(place_id, lookup: :google_places_details)
+    place_types = response.first.types
+    if place_types.nil? || place_types.length == 0
+      return business_type_mapping['service']
+    end
+    # https://developers.google.com/places/web-service/supported_types
+    case place_types.first
+    when 'bar'
+      return business_type_mapping['bar']
+    when 'cafe'
+      return business_type_mapping['cafe']
+    when 'convenience_store', 'liquor_store'
+      return business_type_mapping['late_shop']
+    when 'bakery', 'clothing_store', 'drugstore', 'florist', 'home_goods_store', 'shoe_store', 'store'
+      return business_type_mapping['shop']
+    when 'night_club'
+      return business_type_mapping['club']
+    when 'restaurant'
+      return business_type_mapping['restaurant']
+    when 'hair_care', 'laundry'
+      return business_type_mapping['service']
+    else
+      return business_type_mapping['service']
+    end
+  end
+
+  def geo_params(business_type_mapping, name, street, city, postcode)
+    results = Geocoder.search("#{name} #{street} #{city} #{postcode}", lookup: :google)
     if results.one?
       result = results.first
       {
+        business_type_id: determine_business_type(business_type_mapping, result.place_id),
         lat: result.coordinates[0],
         lng: result.coordinates[1],
         gmap_id: result.place_id,
