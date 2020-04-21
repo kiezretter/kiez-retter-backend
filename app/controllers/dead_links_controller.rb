f1045cbccfffd44c7b3e30a078ff1a8e68a842ac
thohee <thomas.heer@posteo.de>
2020-04-21 22:10:12 +0200

    separate controller for cleanups

--- app/controllers/dead_links_controller.rb
+++ app/controllers/dead_links_controller.rb
@@ -7,20 +7,10 @@ class DeadLinksController < ApplicationController
     @dead_links = DeadLink.all.includes(:funding)
   end
 
-  def update_business_types
-    BusinessTypeUpdateJob.perform_later
-    redirect_to dead_links_path, notice: 'Update job has been started.'
-  end
-
   def destroy
     return unless @dead_link.destroy
 
     redirect_to dead_links_url, notice: 'Dead link was successfully destroyed.'
-  end
-
-  def check_dead_links
-    LinkCheckJob.perform_later
-    redirect_to dead_links_path, notice: 'Link Checker ist gestartet.'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,11 @@ Rails.application.routes.draw do
   devise_for :admins, skip: [:registrations]
   authenticate :admin do
     root to: 'businesses#index'
-    get '/check_dead_links', to: 'dead_links#check_dead_links'
+    get '/check_dead_links', to: 'cleanups#check_dead_links'
     get '/check_duplicates', to: 'businesses#check_duplicates'
-    get '/update_business_types', to: 'dead_links#update_business_types'
+    get '/update_business_types', to: 'cleanups#update_business_types'
     resources :business_types
+    resources :cleanups
     resources :dead_links, only: %i[index destroy]
     resources :businesses do
       member do
